86fd1d38f597767446d0dc6838c46f54569445a9
0214kbs <0214kbs@naver.com>
2024-12-06 16:54:00 +0900

    Feat: reset css 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/globals.css";
+import "../styles/reset.css";
 import localFont from "next/font/local";
 
 const regular = localFont({
