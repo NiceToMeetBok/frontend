57aa35142efad8d3cfddae6b586f6b082703771e
0214kbs <0214kbs@naver.com>
2024-12-11 17:01:45 +0900

    Feat: layout에 title 부분 설정

--- src/app/(route)/signup/layout.tsx
+++ src/app/(route)/signup/layout.tsx
@@ -1,7 +1,25 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "만반잘복: 만나서 반가워. 새해도 잘 부탁해. 복 많이 받아",
+  description: "회원가입 페이지입니다.",
+  icons: {
+    icon: "/bambok.png",
+  },
+};
+
 export default function SignUpLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div>{children}</div>
-    </>
+    <div className="pl-[5vw]">
+      <header className="flex h-[15vh] items-center">
+        <div className="font-bold text-3xl">
+          <span> 나만의</span>
+          <span className="text-primary"> 뱀복이</span>
+          <span> 만들기</span>
+        </div>
+      </header>
+
+      <div className="flex flex-col">{children}</div>
+    </div>
   );
 }
