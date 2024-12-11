76cd42bedfc5b42651eaaee6d47fddd7c03d8c3a
0214kbs <0214kbs@naver.com>
2024-12-11 17:04:50 +0900

    Design: 사용자 닉네임 입력 부분 디자인 적용 - shadcn input 수정

--- src/app/(route)/signup/page.tsx
+++ src/app/(route)/signup/page.tsx
@@ -4,12 +4,18 @@ import { Label } from "@/components/ui/label";
 export default function Page() {
   return (
     <>
-      <div>
-        <div>나만의 뱀복이 만들기</div>
-      </div>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label>사용하실 닉네임을 작성해주세요</Label>
-        <Input placeholder="ex) 보빈" />
+      <div className="flex w-[90%] flex-col justify-center gap-2">
+        <Label>
+          <div className="text-lg">
+            <span>사용하실</span>
+            <span className="text-primary"> 닉네임</span>
+            <span>을 작성해주세요</span>
+          </div>
+        </Label>
+        <div className="flex w-full items-center gap-2 rounded-md border-2 border-secondary px-3 py-2">
+          <Input className="min-w-0 flex-1" placeholder="ex) 보빈" />
+          <span className="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">님의 뱀복이</span>
+        </div>
       </div>
     </>
   );
