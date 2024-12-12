957d1b8ff586315536056b5844466e824ec89fc4
0214kbs <0214kbs@naver.com>
2024-12-13 00:05:30 +0900

    Refactor: Barrel export 패턴 적용

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,14 @@
+export { Input } from "@/components/ui/input";
+export { Button } from "@/components/ui/button";
+export { Checkbox } from "@/components/ui/checkbox";
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "@/components/ui/form";
+export { Label } from "@/components/ui/label";
