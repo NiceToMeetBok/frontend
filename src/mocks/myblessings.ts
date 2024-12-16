1a456f9e76bd4d1810456702cde2e77b56a1a360
0214kbs <0214kbs@naver.com>
2024-12-16 14:53:23 +0900

    Feat: 임시 데이터 생성 및 복주머니 이미지 추가

---
+++ src/mocks/myblessings.ts
@@ -0,0 +1,40 @@
+import { Blessing } from "@/types/blessings";
+
+export const blessings: Blessing[] = [
+  {
+    id: "1",
+    nickname: "남기는자1",
+    luckybagID: 3,
+    message: "새해 복 많이 받아!",
+  },
+  {
+    id: "2",
+    nickname: "남기는자2",
+    luckybagID: 5,
+    message: "안농 ㅋㅋㅋㅋ 올해도 수고 많았고 내년에도 잘해보자",
+  },
+  {
+    id: "3",
+    nickname: "친구A",
+    luckybagID: 9,
+    message: "2024년 정말 빨리 지나갔다.. 2025년도도 화이팅!",
+  },
+  {
+    id: "4",
+    nickname: "친구B",
+    luckybagID: 1,
+    message: "2새해 복 많이 받으세요 :) ",
+  },
+  {
+    id: "5",
+    nickname: "로아",
+    luckybagID: 10,
+    message: "올해도 고생 많았어. 내년에도 파이팅!!",
+  },
+  {
+    id: "6",
+    nickname: "로안",
+    luckybagID: 12,
+    message: "벌써 2025년이네.. 내년에도 잘 부탁해 ",
+  },
+];
