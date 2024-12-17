3b15ef63e3301d209645854e9a2bb9f49a6c4e6b
0214kbs <0214kbs@naver.com>
2024-12-17 15:40:40 +0900

    Design: 미리보기 부분 디자인 적용 - 편지지 이미지 추가 - enter 도 보이도록 처리 - 길어질 경우, scroll 처리

--- src/components/create/steps/preview.tsx
+++ src/components/create/steps/preview.tsx
@@ -10,11 +10,23 @@ type PreviewProps = {
 
 export default function Preview({ onPrevious, onNext, getValues }: PreviewProps) {
   const { nickname, message, luckybagid } = getValues();
+  const messageLines = message.split("\n");
 
   return (
-    <div>
-      <div>미리보기</div>
-      <div>{message}</div>
+    <div className="flex h-full flex-col justify-between gap-7">
+      <div className="text-lg">보빈님에게 아래와 같이 남겨져요!</div>
+
+      <div className="relative h-full w-full">
+        <img src="/letter.svg" className="absolute h-full w-full" alt="편지지" />
+        <div className="absolute left-[50%] top-[50%] h-[70%] w-[70%] -translate-x-1/2 -translate-y-1/2 overflow-y-auto">
+          {messageLines.map((line, index) => (
+            <div key={index} className="whitespace-pre-wrap break-words py-1">
+              {line.trim() !== "" ? line : <br />}
+            </div>
+          ))}
+          <div className="border-t-2 border-[#3C5ABE] p-2 text-right"> from. {nickname}</div>
+        </div>
+      </div>
 
       <div className="flex w-full flex-row justify-between">
         <Button onClick={onPrevious} className="h-16 w-[32%] rounded-lg bg-quaternary text-xl">
