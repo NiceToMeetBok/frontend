9aabf89496ce68ba88c7ddf0581dba845196a260
0214kbs <0214kbs@naver.com>
2024-12-17 16:22:37 +0900

    Design: 편지지에 메시지 위치 조정 - 스크롤이 없을 때 메시지들이 가운데 오도록 - 메시지의 마지막 줄바꿈 없애기

--- src/components/create/steps/preview.tsx
+++ src/components/create/steps/preview.tsx
@@ -10,7 +10,7 @@ type PreviewProps = {
 
 export default function Preview({ onPrevious, onNext, getValues }: PreviewProps) {
   const { nickname, message, luckybagid } = getValues();
-  const messageLines = message.split("\n");
+  const messageLines = message.trim().split("\n");
 
   return (
     <div className="flex h-full flex-col justify-between gap-7">
@@ -18,13 +18,20 @@ export default function Preview({ onPrevious, onNext, getValues }: PreviewProps)
 
       <div className="relative h-full w-full">
         <img src="/letter.svg" className="absolute h-full w-full" alt="편지지" />
-        <div className="absolute left-[50%] top-[50%] h-[70%] w-[70%] -translate-x-1/2 -translate-y-1/2 overflow-y-auto">
-          {messageLines.map((line, index) => (
-            <div key={index} className="whitespace-pre-wrap break-words py-1">
-              {line.trim() !== "" ? line : <br />}
+
+        <div className="absolute left-1/2 top-1/2 box-border flex h-[60%] w-[70%] -translate-x-1/2 -translate-y-1/2 p-6">
+          <div className="relative flex h-full w-full items-center justify-center overflow-hidden">
+            <div className="max-h-full w-full overflow-y-auto">
+              {messageLines.map((line, index) => (
+                <div key={index} className="whitespace-pre-wrap break-words py-1 text-center">
+                  {line.trim() !== "" ? line : <br />}
+                </div>
+              ))}
+              <div className="mt-4 w-full border-t-2 border-[#3C5ABE] p-2 text-right">
+                from. {nickname}
+              </div>
             </div>
-          ))}
-          <div className="border-t-2 border-[#3C5ABE] p-2 text-right"> from. {nickname}</div>
+          </div>
         </div>
       </div>
 
