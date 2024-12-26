bfb0f02c9f29367ff313a488de1ba7ad1f698e90
0214kbs <0214kbs@naver.com>
2024-12-26 20:30:07 +0900

    Fix: metadataBase 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,6 @@ export const metadata: Metadata = {
   icons: {
     icon: "/bambok.png",
   },
-  metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`),
-
   openGraph: {
     title: "만반잘복: 만나서 반가워. 새해도 잘 부탁해. 복 많이 받아",
     description: "소중한 사람들에게 마음을 전할 수 있는 온라인 롤링 페이퍼 서비스 입니다.",
