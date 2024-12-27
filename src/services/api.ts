6386a19173498f31a015c37a56a07ab72ba5a930
0214kbs <0214kbs@naver.com>
2024-12-27 21:32:12 +0900

    Feat: 503에러 발생시 동일한 요청 3번까지 실행

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,10 +12,19 @@ apiClient.interceptors.response.use(
     return response.data;
   },
   (error) => {
+    const originalRequest = error.config;
+
     if (error.response?.status === 401 || error.response?.status === 403) {
       window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/`;
     } else if (error.response?.status === 404) {
       window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/404`;
+    } else if (error.response?.status === 530) {
+      originalRequest._retryCount = originalRequest._retryCount || 0;
+
+      if (originalRequest._retryCount < 3) {
+        originalRequest._retryCount += 1;
+        return apiClient(originalRequest);
+      }
     }
     return Promise.reject(error);
   },
